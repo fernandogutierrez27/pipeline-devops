dccc80be3127de32e5f886ba9f933c777605543f
Fernando Gutiérrez <fernando.gutierrez@live.cl>
2022-01-29 11:06:26 -0300

    modificando ejecucion para opciones

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -13,89 +13,40 @@ def call(){
 
     pipeline {
         agent any
-
-        triggers {
-            githubPush()
-        }
-
         environment {
-            ARTIFACT_VERSION = '0.0.7'
+            ARTIFACT_VERSION = '2.0.0'
         }
-
+        parameters {
+            choice(
+                name:'compileTool',
+                choices: ['Maven', 'Gradle'],
+                description: 'Seleccione herramienta de compilacion'
+            )
+        }
         stages {
-            stage("Paso 1: Build && Test"){
+            stage("Pipeline"){
                 steps {
-                    sh "echo 'Build && Test!'"
-                    sh "gradle clean build"
-                }
-            }
-            stage("Paso 2: Sonar - Análisis Estático"){
-                steps {
-                    sh "echo 'Análisis Estático!'"
-                    withSonarQubeEnv('SonarQubeUsach') {
-                        sh "echo 'SonarQube Analysis!'"
-                        // Run Maven on a Unix agent.
-                        sh 'gradle sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+                    script{
+                        switch(params.compileTool) {
+                            case 'Maven':
+                                def ejecucion = load 'maven.groovy'
+                                ejecucion.call()
+                            break;
+                            case 'Gradle':
+                                def ejecucion = load 'gradle.groovy'
+                                ejecucion.call()
+                            break;
+                        }
                     }
                 }
-            }
-            stage("Paso 3: Curl Springboot Gradle sleep 20"){
-                steps {
-                    sh "gradle bootRun&"
-                    sh "sleep 20 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
-                }
-            }
-            stage("Paso 4: Subir Nexus"){
-                steps {
-                    nexusPublisher nexusInstanceId: 'Nexus',
-                    nexusRepositoryId: 'devops-usach-nexus',
-                    packages: [
-                        [
-                            $class: 'MavenPackage',
-                            mavenAssetList: [
-                                [
-                                    classifier: '',
-                                    extension: '',
-                                    filePath: 'build/libs/DevOpsUsach2022-0.0.0.jar']
-                                ],
-                            mavenCoordinate: [
-                                artifactId: 'DevOpsUsach2022',
-                                groupId: 'com.devopsusach2022',
-                                packaging: 'jar',
-                                version: ARTIFACT_VERSION
-                            ]
-                        ]
-                    ]
-                }
-            }
-            stage("Paso 5: Descargar Nexus"){
-                steps {
-                    withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
-                        sh 'curl -X GET -u $USER:$PASSWORD https://nexus.devopslab.cl/repository/devops-usach-nexus/com/devopsusach2022/DevOpsUsach2022/${ARTIFACT_VERSION}/DevOpsUsach2022-${ARTIFACT_VERSION}.jar -O'
-                        sh "ls"
-                    }
-                }
-            }
-            stage("Paso 6: Levantar Artefacto Jar"){
-                steps {
-                    sh 'nohup java -jar DevOpsUsach2022-${ARTIFACT_VERSION}.jar & >/dev/null'
-                }
-            }
-            stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
-                steps {
-                    sh "sleep 20 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
-                }
-            }
-        }
-        post {
-            always {
-                sh "echo 'fase always executed post'"
-            }
-            success {
-                sh "echo 'fase success'"
-            }
-            failure {
-                sh "echo 'fase failure'"
+                // post{
+                //     success{
+                //         slackSend color: 'good', message: "[Su Nombre] [${JOB_NAME}] [${BUILD_TAG}] Ejecucion Exitosa", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'token-slack'
+                //     }
+                //     failure{
+                //         slackSend color: 'danger', message: "[Su Nombre] [${env.JOB_NAME}] [${BUILD_TAG}] Ejecucion fallida en stage [${env.TAREA}]", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'token-slack'
+                //     }
+                // }
             }
         }
     }
